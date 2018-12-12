9366119cc89658666c18bb6294ce5d47f3ea8b29
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2018-12-12 21:14:39 +0200

    studied classes, variables and primitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,12 +2,36 @@ package org.fasttrackit;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+// this is a comment
+public class App {
+    public static void main(String[] args) {
+        // declaring a variable and instantiating a Car object
+        // this is also called initializing a variable
+        Car firstCompetitor = new Car();
+        firstCompetitor.name = "Dacia";
+        firstCompetitor.color = "black";
+        firstCompetitor.doorCount = 5;
+        firstCompetitor.mileage = 9.5;
+
+        // local variables are variables declared inside a method
+        // they don't receive default values
+        Car secondCompetitor = new Car();
+        secondCompetitor.name = "Golf";
+
+        Engine firstEngine = new Engine();
+        firstEngine.manufacturer = "Renault";
+
+        // sout prints a message to the console
+        System.out.println(firstEngine.manufacturer);
+
+        firstCompetitor.engine = firstEngine;
+
+        firstCompetitor.engine.manufacturer = "BMW";
+
+        System.out.println(firstEngine.manufacturer);
+        System.out.println(firstCompetitor.engine.manufacturer);
+
     }
 }
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,21 @@
+package org.fasttrackit;
+
+public class Car {
+
+    // instance variables (declared as properties of a class)
+    // they receive default values (null for objects, 0 for numbers,
+    // false for booleans)
+    String name;
+    String color;
+    int doorCount;
+    double mileage;
+    boolean running;
+    long totalKm;
+    Engine engine;
+
+
+
+
+
+
+}
